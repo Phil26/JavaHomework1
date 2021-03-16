f2a625e53e14648d7d4c00f42bc45f256893a5aa
Phil26 <63792878+Phil26@users.noreply.github.com>
2021-03-16 13:57:51 +0200

    Add files via upload

---
+++ Homework.Java-1.5/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		Wheel wheel = new Wheel();
+		wheel.goTo();
+	}
+	
+}
---
+++ Homework.Java-1.5/src/Wheel.java
@@ -0,0 +1,17 @@
+
+public class Wheel implements Mechanism 
+{
+ 
+	public void move()
+	{
+		System.out.println("Aceasta este o implementare a metodei 'move()'.");
+	}
+	
+	public void goTo()
+	{
+	   move();
+	}
+	
+	
+	
+}
