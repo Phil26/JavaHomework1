3a4889efd19c8eddb5b6bd008500d22c8429f940
Phil26 <63792878+Phil26@users.noreply.github.com>
2021-03-20 12:35:05 +0200

    Add files via upload

---
+++ Homework.Java-1.20/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.lang.Exception;
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{	
+	float t1 = 0;
+	float t2 = 0;
+	
+	try
+	{
+	System.out.println("Introduceti termenii");
+	System.out.println("Termen 1:");
+	Scanner scanner = new Scanner(System.in);
+	t1 = scanner.nextFloat();
+	System.out.println("Termen 2:");
+	t2 = scanner.nextFloat();
+	System.out.println("Suma termenilor t1 = " + t1 + " si t2 = " + t2 + " este " + Sum.Sum(t1, t2) + ".");
+	}
+	
+	catch(InputMismatchException e)
+	{
+		System.out.println("Atentie: Termenii introdusi nu corespund formatului!");
+		System.out.println("Suma = " + null);
+		e.printStackTrace();
+	}
+	
+	catch(Exception e)
+	{
+		System.out.println("An error has occurred.");
+		System.out.println("Suma = " + null);
+		e.printStackTrace();
+	}
+	
+	}
+
+}
---
+++ Homework.Java-1.20/src/Sum.java
@@ -0,0 +1,12 @@
+import java.util.InputMismatchException;
+import java.lang.Exception;
+
+public class Sum 
+{
+
+	static float Sum(float num1, float num2) throws InputMismatchException, Exception
+	{
+		return num1 + num2;	
+	}
+	
+}
