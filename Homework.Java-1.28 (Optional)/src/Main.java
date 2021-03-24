6f8d3245ed07a9e928aad868c64ebbf7c3e390ac
Phil26 <63792878+Phil26@users.noreply.github.com>
2021-03-25 00:47:31 +0200

    Add files via upload

---
+++ Homework.Java-1.28 (Optional)/src/Main.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.lang.InterruptedException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileWriter;
+
+public class Main 
+{
+
+	public static void main(String[] args) throws IOException, InterruptedException
+	{
+		var url = "http://api.openweathermap.org/data/2.5/air_pollution?lat=44.318378&lon=23.796400&appid=1593c20fe1be2f0058d97f6daf07f49d";
+		var request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+		var client = HttpClient.newBuilder().build();
+		var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		
+		GsonBuilder builder = new GsonBuilder();
+		Gson gson = builder.create();
+		String up = response.body().toUpperCase();
+	    String json = gson.toJson(up);
+	    
+	    FileWriter file = new FileWriter("weather.txt");
+	    file.write(json);
+	    file.close();
+	    
+	}
+	
+}
